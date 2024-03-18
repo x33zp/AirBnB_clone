75b2d93a46e84a42c2f47c1a9a1ec7aa555129a2
zubby peculiar <zubbypeculiar@gmail.com>
2024-03-18 14:47:48 +0100

    fix(base_model): pycodestyle

--- models/base_model.py
+++ models/base_model.py
@@ -21,7 +21,7 @@ class BaseModel:
     def __init__(self, *args, **kwargs):
         """
         Initialize a new instance of the BaseModel class.
-        """ 
+        """
         self.id = str(uuid.uuid4())
         self.created_at = datetime.now()
         self.updated_at = datetime.now()
