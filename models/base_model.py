5900d8f90b16a91fb4a3e013d25de3de3e6157cb
zubby peculiar <zubbypeculiar@gmail.com>
2024-03-18 12:29:46 +0100

    Added: base_model
    This commit implements the BaseModel class, which serves as the foundation for creating other models. The class defines common attributes and methods such as id generation, timestamp management, and conversion to dictionary format

---
+++ models/base_model.py
@@ -0,0 +1,63 @@
+#!/usr/bin/python
+"""BaseModel module.
+
+This module defines the BaseModel class, which defines all
+common attributes/methods for other classes.
+"""
+
+import uuid
+from datetime import datetime
+
+
+class BaseModel:
+    """A base class for creating models with unique identifiers and timestamps.
+
+    Public instance attributes:
+    - id: str - A unique identifier generated using uuid.uuid4().
+    - created_at: datetime - The datetime when the instance is created.
+    - updated_at: datetime - The datetime when the instance is updated.
+    """
+
+    def __init__(self):
+        """
+        Initialize a new instance of the BaseModel class.
+        """
+        self.id = str(uuid.uuid4())
+        self.created_at = datetime.now()
+        self.updated_at = datetime.now()
+
+    def __str__(self):
+        """Return a string representation of the object.
+
+        formatted as:
+        - "[ClassName] (id) {'attribute1': value1, 'attribute2': value2, ...}".
+        """
+        return "[{}] ({}) {}".format(type(self).__name__, self.id,
+                                     self.__dict__)
+
+    def save(self):
+        """Update the 'updated_at' attribute with the current datetime.
+
+        This method updates the 'updated_at' attribute of the instance with the
+        current datetime when it is called. It is typically used to indicate
+        that the object has been modified or updated.
+        """
+        self.updated_at = datetime.now()
+
+    def to_dict(self):
+        """Return a dictionary representation of the instance.
+
+        This method converts the instance into a dictionary representation:
+        - The keys are the instance attributes (excluding private and special
+          attributes).
+        - The values are the corresponding values of the instance attributes.
+        - An additional key '__class__' is added with the class name of the
+          object.
+        - The 'created_at' and 'updated_at' attributes are converted to ISO
+          format strings.
+        """
+        obj_dict = self.__dict__.copy()
+        obj_dict['__class__'] = type(self).__name__
+        obj_dict['created_at'] = self.created_at.isoformat()
+        obj_dict['updated_at'] = self.updated_at.isoformat()
+        return obj_dict
