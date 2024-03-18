2e37f4e54bd84a31f7f812ff066a3c18fb6ec97e
zubby peculiar <zubbypeculiar@gmail.com>
2024-03-18 14:45:01 +0100

    updated: __init method is base_model
    Refactored __init__ to accept *args and **kwargs, **kwargs takes a dictionary representation and recreates the instance, Convert created_at and updated_at strings to datetime objects when present

--- models/base_model.py
+++ models/base_model.py
@@ -21,10 +21,18 @@ class BaseModel:
     def __init__(self, *args, **kwargs):
         """
         Initialize a new instance of the BaseModel class.
-        """
+        """ 
         self.id = str(uuid.uuid4())
         self.created_at = datetime.now()
         self.updated_at = datetime.now()
+
+        if kwargs:
+            timeformat = "%Y-%m-%dT%H:%M:%S.%f"
+            for key, value in kwargs.items():
+                if key == 'created_at' or key == 'updated_at':
+                    value = datetime.strptime(kwargs[key], timeformat)
+                if key != '__class__':
+                    setattr(self, key, value)        
 
     def __str__(self):
         """Return a string representation of the object.
