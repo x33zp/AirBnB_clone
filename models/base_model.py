e93427b3fa4f3ad7f88e01d8ed260d78990117e5
zubby peculiar <zubbypeculiar@gmail.com>
2024-03-19 05:02:13 +0100

    fix(base_model): __init__
    fixed the kwargs if and else to use kwargs if provided else assign the provided values

--- models/base_model.py
+++ models/base_model.py
@@ -23,12 +23,12 @@ class BaseModel:
         """
         Initialize a new instance of the BaseModel class.
         """
-        self.id = str(uuid.uuid4())
-        self.created_at = datetime.now()
-        self.updated_at = datetime.now()
-        models.storage.new(self)
-
-        if kwargs:
+        if not kwargs:
+            self.id = str(uuid.uuid4())
+            self.created_at = datetime.now()
+            self.updated_at = datetime.now()
+            models.storage.new(self)
+        else:
             timeformat = "%Y-%m-%dT%H:%M:%S.%f"
             for key, value in kwargs.items():
                 if key == 'created_at' or key == 'updated_at':
