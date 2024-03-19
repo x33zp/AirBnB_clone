ab64943346804faf35d529034b4f9c9c000f01b3
zubbypeculiar <zubbypeculiar@gmail.com>
2024-03-19 16:11:15 +0000

    Added(Review): Class module
    module that defines a Review class

---
+++ models/review.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""Review module.
+
+This module which inherits from BaseModel defines the State
+class, which represents the reviews given by the User.
+"""
+
+from models.base_model import BaseModel
+
+
+class Review(BaseModel):
+    """This class represents Review."""
+    place_id = ""
+    user_id = ""
+    text = ""
