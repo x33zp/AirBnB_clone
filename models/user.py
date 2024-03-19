5c3d14d4e66d45cfd9523403de2ba60252a095f4
zubbypeculiar <zubbypeculiar@gmail.com>
2024-03-19 15:13:18 +0000

    Added(user): User class
    User class represents a user, with public instance atrr; email, password, first_name and  last_name

---
+++ models/user.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python
+"""User module.
+
+This module which inherits from BaseModel defines the User
+class, which represents a user of the application.
+"""
+
+
+class User():
+    """This class represents a user."""
+    email = ""
+    password = ""
+    first_name = ""
+    last_name = ""
