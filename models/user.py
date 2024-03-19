4dda789e07d6aaa91a133b2c915e505a5e919cce
zubbypeculiar <zubbypeculiar@gmail.com>
2024-03-19 15:20:14 +0000

    updated(User): import
    imported BaseModel

--- models/user.py
+++ models/user.py
@@ -5,6 +5,8 @@ This module which inherits from BaseModel defines the User
 class, which represents a user of the application.
 """
 
+from models.base_model import BaseModel
+
 
 class User():
     """This class represents a user."""
