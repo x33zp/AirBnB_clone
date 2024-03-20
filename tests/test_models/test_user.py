045fbcffa46cdccf39a8a41cb01b3b5f8e0f859e
zubby peculiar <zubbypeculiar@gmail.com>
2024-03-20 16:42:27 +0100

    Added(User): test_user model
    test case for the user class

---
+++ tests/test_models/test_user.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""Unit tests for the User class.
+
+This module contains unit tests for the User class.
+"""
+
+import os
+import unittest
+from models.base_model import BaseModel
+
+class TestUser(unittest.TestCase):
+    """Test cases for the User class."""
+    pass
