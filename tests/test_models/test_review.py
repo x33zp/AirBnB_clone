972190929f05c2e238632aac08d3734100e77661
zubby peculiar <zubbypeculiar@gmail.com>
2024-03-21 13:00:55 +0100

    Added(test_review): unittest
    unittest for the review model

---
+++ tests/test_models/test_review.py
@@ -0,0 +1,113 @@
+#!/usr/bin/python3
+"""Unit tests for the User class.
+
+This module contains unit tests for the User class.
+"""
+
+import os
+import unittest
+from models.review import Review
+from datetime import datetime
+from models.base_model import BaseModel
+
+
+class TestUser(unittest.TestCase):
+    """Test cases for the User class."""
+
+    def setUp(self):
+        """Set up method for thr class"""
+        self.obj = Review()
+        self.obj_2 = Review()
+        self.obj.place_id = "1234"
+        self.obj_dict = self.obj.to_dict()
+
+        if os.path.isfile("file.json"):
+            os.rename("file.json", "tmp.json")
+
+    def tearDown(self):
+        """Tear down method for the class."""
+        if os.path.isfile("file.json"):
+            os.remove("file.json")
+        elif os.path.isfile("tmp.json"):
+            os.rename("tmp.json", "file.json")
+
+    def test_init(self):
+        """Test initialization of BaseModel."""
+        self.assertIsNotNone(self.obj.id)
+        self.assertIsNotNone(self.obj.created_at)
+        self.assertIsNotNone(self.obj.updated_at)
+        self.assertTrue(issubclass(type(self.obj), BaseModel))
+
+    def test_uuid(self):
+        """Test uniqueness of UUID."""
+        self.assertTrue(hasattr(self.obj, 'id'))
+        self.assertNotEqual(self.obj.id, self.obj_2.id)
+        self.assertIsInstance(self.obj.id, str)
+
+    def test_datetime(self):
+        """Test datetime attributes."""
+        self.assertTrue(hasattr(self.obj, 'created_at'))
+        self.assertTrue(hasattr(self.obj, 'updated_at'))
+        self.assertIsInstance(self.obj.created_at, datetime)
+        self.assertIsInstance(self.obj.updated_at, datetime)
+        self.assertNotEqual(self.obj.created_at,
+                            self.obj_2.created_at)
+
+    def test_for_attributes(self):
+        """Test if class attributes exists."""
+        self.assertIn('user_id', dir(self.obj))
+        self.assertTrue(hasattr(self.obj, 'place_id'))
+        self.assertTrue(hasattr(self.obj, 'user_id'))
+        self.assertTrue(hasattr(self.obj, 'text'))
+        self.assertNotIn('user_id', self.obj.__dict__)
+
+    def test_for_attr_types(self):
+        """Test for attribute types"""
+        self.assertIsInstance(self.obj.place_id, str)
+        self.assertIsInstance(self.obj.user_id, str)
+        self.assertIsInstance(self.obj.text, str)
+
+    def test_init_with_attribute(self):
+        """Test initialization with additional attribute."""
+        self.obj.state_id = "10011"
+        self.obj.text = "test_init_attr"
+        self.assertEqual(self.obj.state_id, "10011")
+        self.assertEqual(self.obj.text, "test_init_attr")
+
+    def test_str_representation(self):
+        """Test string representation."""
+        self.obj.name = "test_str"
+        expected_str = "[Review] ({}) {}".format(self.obj.id,
+                                                 self.obj.__dict__)
+        self.assertEqual(str(self.obj), expected_str)
+
+    def test_dict_representation(self):
+        """Test dictionary representation."""
+        created_iso = self.obj.created_at.isoformat()
+        updated_iso = self.obj.updated_at.isoformat()
+        self.assertEqual(self.obj_dict['id'], self.obj.id)
+        self.assertEqual(self.obj_dict['__class__'], 'Review')
+        self.assertEqual(self.obj_dict['place_id'], "1234")
+        self.assertEqual(self.obj_dict['created_at'], created_iso)
+        self.assertEqual(self.obj_dict['updated_at'], updated_iso)
+
+    def test_save(self):
+        """Test save method."""
+        self.obj.save()
+        obj_dict_2 = self.obj.to_dict()
+        self.assertNotEqual(self.obj_dict['updated_at'],
+                            obj_dict_2['updated_at'])
+        self.assertEqual(self.obj_dict['created_at'],
+                         obj_dict_2['created_at'])
+
+    def test_init_with_kwargs(self):
+        """Test initialization with keyword arguments."""
+        kwargs_model = Review(**self.obj_dict)
+        self.assertIsInstance(kwargs_model.id, str)
+        self.assertEqual(kwargs_model.place_id, "1234")
+        self.assertIsInstance(kwargs_model.created_at, datetime)
+        self.assertIsNotNone(kwargs_model.updated_at, datetime)
+
+
+if __name__ == '__main__':
+    unittest.main()
