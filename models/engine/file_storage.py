4514b2c3b5c1dc57cd31d91d6983c5d4c783ea07
zubby peculiar <zubbypeculiar@gmail.com>
2024-03-23 17:18:47 +0100

    revert

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -1,52 +1,59 @@
 #!/usr/bin/python3
-""" A FileStorage class """
+"""The manages serialization and deserialization
+of instances to/from JSON file.
+"""
+
+import os
 import json
-from models.base_model import BaseModel
-from models.user import User
-from models.state import State
 from models.city import City
-from models.amenity import Amenity
+from models.user import User
 from models.place import Place
+from models.state import State
 from models.review import Review
+from models.amenity import Amenity
+from models.base_model import BaseModel
 
 
 class FileStorage:
-    """ A class FileStorage that serializes instances to a
-        JSON file and deserializes JSON file to instances:
-
-        Attributes:
-            __file_path (str): The name of the file to save objects to.
-            __objects (dict): A dictionary of instantiated objects.
+    """Class that serializes instances to a JSON file and
+    deserializes JSON file to instances
     """
     __file_path = "file.json"
     __objects = {}
 
     def all(self):
-        """ Returns the dictionary __objects """
+        """Get all instances stored in the file.
+        """
         return self.__objects
 
     def new(self, obj):
-        """ Sets in __objects the obj with key <obj class name>.id """
-        oc_name = obj.__class__.__name__
-        self.__objects["{}.{}".format(oc_name, obj.id)] = obj
+        """Add a new instance to the storage.
+        """
+        key = "{}.{}".format(type(obj).__name__, obj.id)
+        self.__objects[key] = obj
 
     def save(self):
-        """ Serializes __objects to the JSON file (path: __file_path) """
+        """Saves the serialized instances to the JSON file.
+        """
+        filename = self.__file_path
         json_data = {}
-        for k, v in self.__objects.items():
-            json_data[k] = v.to_dict()
-        with open(self.__file_path, 'w') as jsonf:
-            json.dump(json_data, jsonf)
+
+        for key, value in self.__objects.items():
+            json_data[key] = value.to_dict()
+
+        with open(filename, 'w') as jsonfile:
+            json.dump(json_data, jsonfile)
 
     def reload(self):
-        """ Deserializes the JSON file to __objects (only if the JSON file
-            (__file_path) exists ; otherwise, do nothing. If the file doesn’t
-            exist, no exception should be raised) """
-        try:
-            with open(self.__file_path) as jsonf:
-                data = json.load(jsonf)
-                for key, obj in data.items():
-                    newObj = eval(obj["__class__"])(**obj)
-                    self.__objects[key] = newObj
-        except FileNotFoundError:
+        """Deserialize the JSON file and reload instances into memory.
+        """
+        filename = self.__file_path
+
+        if not os.path.isfile(self.__file_path):
             return
+
+        with open(filename, 'r') as jsonfile:
+            obj_dict = json.load(jsonfile)
+            for key, value in obj_dict.items():
+                class_obj = eval(value["__class__"])(**value)
+                self.__objects[key] = class_obj
