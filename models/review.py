a21922fe0e2a6b05e92dad39b10adc4b9ded1f22
zubby peculiar <zubbypeculiar@gmail.com>
2024-03-25 03:04:46 +0100

    fix

--- models/review.py
+++ models/review.py
@@ -1,12 +1,16 @@
 #!/usr/bin/python3
-"""This module creates a Review class"""
+"""Review module.
+
+This module which inherits from BaseModel defines the State
+class, which represents the reviews given by the User.
+"""
 
 from models.base_model import BaseModel
 
 
 class Review(BaseModel):
-    """Class for managing review objects"""
-
+    """This class represents Review."""
+    
     place_id = ""
     user_id = ""
     text = ""
